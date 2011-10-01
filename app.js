15b0b42ba15b43e31dc69143a2d8994fd88cc304
Aditya <aditya@netroy.in>
2011-10-01 07:04:07 +0530

    updated slides for jsFoo 2011

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (sypres) is running..');
-}).listen(10875);
+var express = require('express'),
+         io = require('socket.io'),
+        app = express.createServer();
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.enable('view cache');
+  app.use(app.router);
+  app.use(express.static(__dirname + '/static'));
+
+  // Catch all unhandled routes ... i dont like 404s
+  app.use(function(eq, resp){
+    resp.redirect("/");
+  });
+
+});
+
+app.get("/[1-9]?([0-9])?/?", function(req, resp){
+  resp.render("main", {
+    "title": "Realtime Web"
+  });
+});
+
+if (!module.parent) {
+  app.listen(process.env.app_port || 10875);
+  console.info("Started on port %d", app.address().port);
+}
+
+var io = io.listen(app);
+io.set('log level', 1);
+io.sockets.on('connection', function(client){
+  // do something
+});
---
+++ static/js/main.js
@@ -0,0 +1,108 @@
+var init = (function(window, document, $){
+  function init(){
+  
+    // handle keys
+    var keyEventMap = {
+      38: "previousSlide",
+      40: "nextSlide",
+      37: "previous",
+      39: "next"
+    }, 
+    slides = $("article"), 
+    pages = $("#pages"),
+    index = 0, 
+    last = slides.length - 1,
+    historyAPISupported = !!("history" in window && history.pushState),
+    isAdmin = true;
+
+    function updateSlide(i, skipHistory){
+      if(typeof i !== 'number' || i < 0 || i > last){
+        index = (i < 0) ? 0 : last;
+        return;
+      }
+
+      $(slides[index]).removeClass("selected").addClass(i > index ? "prev" : "next");
+      $(slides[i]).addClass("selected").removeClass("prev next");
+
+      index = i;
+      if(historyAPISupported && typeof skipHistory === 'undefined'){
+        history.pushState(null, null, "/" + (index+1));
+      }
+      pages.html((index+1) + " / " + (last+1));
+    }
+
+    var actions = {
+      previous: function(){
+        updateSlide(index-1);
+      },
+      next: function(){
+        var li = $(slides[index]).find("li.hidden");
+        if(li.length > 0){
+          li.first().removeClass("hidden");
+        }else{
+          updateSlide(index+1);
+        }
+      },
+      previousSlide: function(){
+
+      },
+      nextSlide: function(){
+
+      }
+    };
+    
+
+    $(document).bind("keydown", function(e){
+      var ev = keyEventMap[e.keyCode];
+      if(typeof ev !== "undefined"){
+        $(document).trigger(ev);
+      }
+    });
+
+
+
+    for(var ev in keyEventMap){
+      ev = keyEventMap[ev];
+      $(document).bind(ev, actions[ev]);
+    }
+    
+    if(historyAPISupported){
+      $(window).bind("popstate", function(e){
+        var index = parseInt(location.pathname.substr(1) || 1, 10);
+        if(!isNaN(index)){
+          updateSlide(index-1, true);
+        }
+      });
+    }
+
+    var current = $(slides[parseInt(location.pathname.substr(1) || 1, 10) - 1]);
+    current.addClass("selected");
+    current.prevAll("article").addClass("prev");
+    jQuery.merge(current.nextAll("article").addClass("next"), current).find("li").addClass("hidden");
+    slides.parent().show();
+
+
+/*
+    (function(callback) {
+      var keys = "38,38,40,40,37,39,37,39,66,65";
+      var strokes = [];
+      $(window).keypress(function(e){
+        strokes.push(e.keyCode);
+        if (strokes.join().indexOf(keys) >= 0){
+          $(document).unbind('keydown', arguments.callee);
+          callback();
+        }
+      }, true);
+    })(function(){
+      isAdmin = true;
+      if(console.log){
+        console.log("You've hit the magic keys, admin now");
+      }
+    });
+*/
+
+
+
+  }
+  $(init);
+})(window, document, jQuery);
